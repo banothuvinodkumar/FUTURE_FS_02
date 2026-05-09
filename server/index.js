69895fc0a2724833f835821ab6b8f2c7a59599ef
banothuvinodkumar <vinodkumarbanothu855@gmail.com>
2026-05-09 11:06:52 +0530

    1st cmt

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, '/client/dist')));
 
-  app.get('*', (req, res) =>
+  app.get(/.*/, (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   );
 } else {
