c0aa138e855063f4b5258a77008a5f4b5405ab08
banothuvinodkumar <vinodkumarbanothu855@gmail.com>
2026-05-09 11:11:02 +0530

    2nd cmt

--- server/index.js
+++ server/index.js
@@ -39,19 +39,10 @@ app.use('/api', limiter);
 app.use('/api/auth', authRoutes);
 app.use('/api/leads', leadRoutes);
 
-// --- PRODUCTION DEPLOYMENT ---
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/client/dist')));
-
-  app.get(/.*/, (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running in development mode...');
-  });
-}
+// Base Route for API Health Check
+app.get('/', (req, res) => {
+  res.send('MiniCRM API is successfully up and running!');
+});
 
 // Error Handler
 app.use(notFound);
